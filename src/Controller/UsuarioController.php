bdf753b60e3dbc0787d3362517adcf713d54c1c6
Diego Quinteros <ql17001@ues.edu.sv>
2023-09-23 16:29:43 -0600

    ✨ Update UsuarioController: Add create

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -2,19 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Usuario;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/usuario', name: 'usuarios')]
 class UsuarioController extends AbstractController
 {
-  #[Route('/{id}', name: 'getUsuario', methods: ['get'])]
-  public function getUsuario(int $id): JsonResponse
+  #[Route('', name: 'app_usuario_create', methods: ['POST'])]
+  public function create(EntityManagerInterface $entityManager, Request $request): JsonResponse
   {
-    // Este codigo simula una busqueda
-    $usuario = ["id"=>$id, "nombre"=>"Juan", "edad"=>30];
+    $usuario = new Usuario();
+    $usuario->setNombre($request->request->get('nombre'));
+    $usuario->setEdad($request->request->get('edad'));
+    // Se avisa a Doctrine que queremos guardar un nuevo registro pero no se ejecutan las consultas
+    $entityManager->persist($usuario);
 
-    return $this->json($usuario);
+    // Se ejecutan las consultas SQL para guardar el nuevo registro
+    $entityManager->flush();
+
+    return $this->json([
+        'message' => 'Se guardo el nuevo usuario con id ' . $usuario->getId()
+    ]); 
   }
 }
